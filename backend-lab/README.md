e9578a528b18bf19cd1c06e85d3384b5c7ea8a54
Zeidra Firdaws <abduhsttmandala@gmail.com>
2023-12-11 16:40:54 +0700

    backend

---
+++ backend-lab/README.md
@@ -0,0 +1,16 @@
+1. Buat folder: backend-lab
+2. ke folder backend-lab
+3. ketikan perintah: npm init -y
+4. npm i express cors dotenv
+5. npm i -D prisma 
+6. npm i @prisma/client
+7. npx prisma init
+8. npx prisma migrate dev
+9. tambahkan "type": "module" pada file package.json untuk es6 sintax
+8. buat file index.js pada folder backend-lab
+9. install nodemon
+10. jalankan express js: node index.js / nodemon index
+11. npx prisma migrate reset
+
+### Tugas
+Buat Category dan Relasinya dengan Tabel Product
---
+++ backend-lab/prisma/migrations/20231211083824_23/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - Added the required column `categoryId` to the `Product` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `product` ADD COLUMN `categoryId` INTEGER NOT NULL;
+
+-- CreateTable
+CREATE TABLE `Category` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Product` ADD CONSTRAINT `Product_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `Category`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
